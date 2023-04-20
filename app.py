2f34b9d8dd0786c7bff9fe0ab979a4f7276cef3a
JoseACortes <cortesjoseandres@gmail.com>
2023-04-20 00:30:02 +0000

    why doesnt this work!!!

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-print('test1')
 import AbreivBot.Functions as fnc
 from AbreivBot.Models.BaseModels import BaseModel
-print('test2')
 import string
 
 possible_chars = string.ascii_letters + string.digits + ' .@'
 
 model = BaseModel(dictionary=possible_chars)
 model.load_weights('AbreivBot/Models/TrainedModels/BaseModel')
-print('test3')
+
 
 from flask import Flask, request, jsonify
-print('test4')
 app = Flask(__name__)
-print('test5')
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    print('a')
     data = request.get_json()
     data = fnc.vecotrize_list(data['input'])
-    print('b')
     
     output = model.predict(data)
     
@@ -33,7 +27,4 @@ def predict():
 import os
 
 if __name__ == '__main__':
-    print('test6')
-    # app.run()
     app.run(port=int(os.environ.get('PORT', 5000)))
-    print('test7')
